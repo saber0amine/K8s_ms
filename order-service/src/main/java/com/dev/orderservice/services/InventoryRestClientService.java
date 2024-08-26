2f8ab24430ca29875e4cc47e1ce8b25226e54ff7
saber0amine <saberamine000@gmail.com>
2024-08-26 15:57:45 +0100

    update service commun to container names

--- order-service/src/main/java/com/dev/orderservice/services/InventoryRestClientService.java
+++ order-service/src/main/java/com/dev/orderservice/services/InventoryRestClientService.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
-@FeignClient(name = "k8s_inventory-service-app_inventory-service-app-865d94966-mffc8_default_c4d40d20-66d6-4cfc-b0b7-b2a09e036257_0")
+@FeignClient(name = "inventory-service-app-865d94966-mffc8")
 public interface InventoryRestClientService {
     @GetMapping("/products/{id} ")
     public Product productById(@PathVariable Long id);
