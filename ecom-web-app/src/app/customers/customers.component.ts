b8df522e583c85c5ee6c252fa826e1bd0b3041b8
saber0amine <saberamine000@gmail.com>
2024-08-29 15:55:45 +0100

    update service commun to container names

--- ecom-web-app/src/app/customers/customers.component.ts
+++ ecom-web-app/src/app/customers/customers.component.ts
@@ -12,7 +12,7 @@ export class CustomersComponent implements OnInit {
   constructor(private http:HttpClient, private router: Router) { }
 
   ngOnInit(): void {
-    this.http.get("http://192.168.49.2:9999/customers")
+    this.http.get("http://192.168.49.2:31769/customers")
       .subscribe({
         next : (data)=>{
           this.customers=data;
