3c89501adfd3118df48399340c2b1bc88422d76d
saber0amine <saberamine000@gmail.com>
2024-08-31 00:23:18 +0100

    add the monitoring stack

--- order-service/src/main/java/com/dev/orderservice/services/InventoryRestClientService.java
+++ order-service/src/main/java/com/dev/orderservice/services/InventoryRestClientService.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import java.util.List;
 
 //url = "http://10.244.0.133:8082" for kubernetes
-@FeignClient(name = "inventory-service" , url = "http://10.244.1.89:8082"  )
+@FeignClient(name = "inventory-service" , url = "http://10.244.1.102:8082"  )
 public interface InventoryRestClientService {
 //    @GetMapping("/products/{id} ")
 //    public Product productById(@PathVariable Long id);
