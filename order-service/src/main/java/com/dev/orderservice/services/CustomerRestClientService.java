c39b005b00c79221b2a03608614724b36720f8ed
saber0amine <saberamine000@gmail.com>
2024-08-30 20:57:06 +0100

    add the monitoring stack

--- order-service/src/main/java/com/dev/orderservice/services/CustomerRestClientService.java
+++ order-service/src/main/java/com/dev/orderservice/services/CustomerRestClientService.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 //, url = "http://10.244.0.131:8081"
 
-@FeignClient(name = "customer-service" , url = "http://10.244.0.244:8081")
+@FeignClient(name = "customer-service" , url = "http://10.244.1.27:8081")
 public interface CustomerRestClientService {
 //    @GetMapping("/customers/{id}")
 //    public Customer customerById(@PathVariable Long id);
--- order-service/src/main/java/com/dev/orderservice/services/InventoryRestClientService.java
+++ order-service/src/main/java/com/dev/orderservice/services/InventoryRestClientService.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import java.util.List;
 
 //url = "http://10.244.0.133:8082" for kubernetes
-@FeignClient(name = "inventory-service" , url = "http://10.244.0.242:8082"  )
+@FeignClient(name = "inventory-service" , url = "http://10.244.1.27:8082"  )
 public interface InventoryRestClientService {
 //    @GetMapping("/products/{id} ")
 //    public Product productById(@PathVariable Long id);
