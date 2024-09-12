e19a9f18ef0f57bc15c782710f97ae7dc06d20e4
saber0amine <saberamine000@gmail.com>
2024-09-12 02:11:31 +0100

    add the keycloak set up

--- geteway-service/src/main/java/com/dev/getewayservice/GetewayServiceApplication.java
+++ geteway-service/src/main/java/com/dev/getewayservice/GetewayServiceApplication.java
@@ -8,8 +8,8 @@ import org.springframework.cloud.client.discovery.ReactiveDiscoveryClient;
 import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
 import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.oauth2.jwt.JwtDecoder;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.oauth2.jwt.JwtDecoder;
 
 @SpringBootApplication
 @EnableDiscoveryClient
